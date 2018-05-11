3e96e196862208d020e5bb00b251ef6f8ed5aa6a
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2018-05-10 18:58:19 -0700

    updated function to use dmp context

--- mysite/homepage/scripts/index.js
+++ mysite/homepage/scripts/index.js
@@ -1,11 +1,14 @@
-$(function() {
-    // update the time every n seconds
-    window.setInterval(function() {
-        $('.browser-time').text('The current browser time is ' + new Date());
-    }, 1000);
+$(function(context) {
+    return function() {
+        // update the time every n seconds
+        window.setInterval(function () {
+            $('.browser-time').text('The current browser time is ' + new Date())
+        }, 1000)
 
-    // update server time button
-    $('#server-time-button').click(function() {
-        $('.server-time').load('/homepage/index.gettime');
-    });
-});
+        // update server time button
+        $('#server-time-button').click(function () {
+            console.log(context)
+            $('.server-time').load('/homepage/index.gettime')
+        })
+    }
+}(DMP_CONTEXT.get()))
